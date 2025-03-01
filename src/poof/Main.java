52ae9c081f1ce948164b401b19914010595660a4
Aidan <aidansavantgeneral@gmail.com>
2025-03-01 15:36:14 -0700

    testing

--- src/poof/Main.java
+++ src/poof/Main.java
@@ -5,5 +5,6 @@ import poof.controller.EntryPointController;
 public class Main {
     public static void main(String[] args) {
         new EntryPointController().run();
+        System.out.println("test");
     }
 }
